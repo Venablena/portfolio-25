6a1a8be102e360683eb614d2fd66685e8494b7d7
Lena Venable <venablena@gmail.com>
2025-11-07 18:58:12 -0800

    configure vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-reagit ct'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio-25/',
+  base: '/',
   plugins: [react()],
 })
